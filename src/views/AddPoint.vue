8e30e4eddc2fecd42fe765e166cc206e22a4f276
LorenzoSavi <129039054+LorenzoSavi@users.noreply.github.com>
2025-05-30 16:49:08 +0000

    v2.20

--- src/views/AddPoint.vue
+++ src/views/AddPoint.vue
@@ -2,14 +2,14 @@
   <!-- Header con titolo e bottone logout -->
   <div class="level mb-3 px-3 pt-2">
     <div class="level-left">
-      <h1 class="title is-2 page-title">Add Point</h1>
+      <h1 class="title is-2 page-title">Aggiungi Punti</h1>
     </div>
     <div class="level-right">
       <button @click="handleLogout" class="button is-danger is-small">
         <span class="icon is-small">
           <i class="fas fa-sign-out-alt"></i>
         </span>
-        <span class="is-hidden-mobile">Logout</span>
+        <span class="is-hidden-mobile">Esci</span>
       </button>
     </div>
   </div>
@@ -18,42 +18,148 @@
     <div class="container">
       <form @submit.prevent="addPoints">
         <div class="field">
-          <label class="label">Select Team:</label>
+          <label class="label">Seleziona Squadra:</label>
           <div class="control">
-            <div class="select is-fullwidth">
-              <select v-model="selectedTeam">
-                <option value="">Scegli un team...</option>
-                <option v-for="team in teams" :key="team.id" :value="team.id" :class="[team.id + '-text', 'team-option']">{{ team.name }}</option>              
-              </select>
+            <div class="custom-dropdown" :class="{ 'is-active': showTeamDropdown }">
+              <div class="dropdown-trigger" @click="toggleTeamDropdown">
+                <div class="dropdown-display">
+                  <span v-if="selectedTeam" :class="[selectedTeam + '-text', 'selected-team']">
+                    {{ teams.find(t => t.id === selectedTeam)?.name }}
+                  </span>
+                  <span v-else class="placeholder">Scegli una squadra...</span>
+                  <span class="dropdown-icon">
+                    <i class="fas fa-chevron-down"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="dropdown-menu" v-show="showTeamDropdown">
+                <div class="dropdown-content">
+                  <div 
+                    v-for="team in teams" 
+                    :key="team.id" 
+                    class="dropdown-item" 
+                    :class="[team.id + '-text', { 'is-active': selectedTeam === team.id }]"
+                    @click="selectTeam(team.id)"
+                  >
+                    {{ team.name }}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="field">
-          <label class="label">Select Game:</label>
+          <label class="label">Seleziona Ciclo:</label>
           <div class="control">
-            <div class="select is-fullwidth">
-              <select v-model="selectedGame">
-                <option value="">Scegli un gioco...</option>
-                <option v-for="game in games" :key="game.id" :value="game.id">{{ game.name }}</option>
-              </select>
+            <div class="custom-dropdown" :class="{ 'is-active': showCycleDropdown }">
+              <div class="dropdown-trigger" @click="toggleCycleDropdown">
+                <div class="dropdown-display">
+                  <span v-if="selectedCycle" class="selected-option">
+                    {{ getCycleName(selectedCycle) }}
+                  </span>
+                  <span v-else class="placeholder">Scegli un ciclo...</span>
+                  <span class="dropdown-icon">
+                    <i class="fas fa-chevron-down"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="dropdown-menu" v-show="showCycleDropdown">
+                <div class="dropdown-content">
+                  <div 
+                    class="dropdown-item" 
+                    :class="{ 'is-active': selectedCycle === 'primo' }"
+                    @click="selectCycle('primo')"
+                  >
+                    Primo Ciclo
+                  </div>
+                  <div 
+                    class="dropdown-item" 
+                    :class="{ 'is-active': selectedCycle === 'secondo' }"
+                    @click="selectCycle('secondo')"
+                  >
+                    Secondo Ciclo
+                  </div>
+                  <div 
+                    class="dropdown-item" 
+                    :class="{ 'is-active': selectedCycle === 'terzo' }"
+                    @click="selectCycle('terzo')"
+                  >
+                    Terzo Ciclo
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="field">
-          <label class="label">Points to Add:</label>
+          <label class="label">Seleziona Gioco:</label>
           <div class="control">
-            <div class="select is-fullwidth">
-              <select v-model.number="pointsToAdd">
-                <option value="0">Scegli punti...</option>
-                <option v-for="point in pointsOptions.filter(p => p > 0)" :key="point" :value="point">{{ point }}</option>
-              </select>
+            <div class="custom-dropdown" :class="{ 'is-active': showGameDropdown, 'is-disabled': !selectedCycle }">
+              <div class="dropdown-trigger" @click="toggleGameDropdown">
+                <div class="dropdown-display">
+                  <span v-if="selectedGame" class="selected-option">
+                    {{ availableGames.find(g => g.id === selectedGame)?.name }}
+                  </span>
+                  <span v-else class="placeholder">
+                    {{ selectedCycle ? 'Scegli un gioco...' : 'Prima seleziona un ciclo' }}
+                  </span>
+                  <span class="dropdown-icon">
+                    <i class="fas fa-chevron-down"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="dropdown-menu" v-show="showGameDropdown && selectedCycle">
+                <div class="dropdown-content">
+                  <div 
+                    v-for="game in availableGames" 
+                    :key="game.id" 
+                    class="dropdown-item" 
+                    :class="{ 'is-active': selectedGame === game.id }"
+                    @click="selectGame(game.id)"
+                  >
+                    {{ game.name }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Punti da Aggiungere:</label>
+          <div class="control">
+            <div class="custom-dropdown" :class="{ 'is-active': showPointsDropdown }">
+              <div class="dropdown-trigger" @click="togglePointsDropdown">
+                <div class="dropdown-display">
+                  <span v-if="pointsToAdd > 0" class="selected-option points-display">
+                    {{ pointsToAdd }} punti
+                  </span>
+                  <span v-else class="placeholder">Scegli i punti...</span>
+                  <span class="dropdown-icon">
+                    <i class="fas fa-chevron-down"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="dropdown-menu" v-show="showPointsDropdown">
+                <div class="dropdown-content">
+                  <div 
+                    v-for="point in pointsOptions.filter(p => p > 0)" 
+                    :key="point" 
+                    class="dropdown-item points-item" 
+                    :class="{ 'is-active': pointsToAdd === point }"
+                    @click="selectPoints(point)"
+                  >
+                    {{ point }} punti
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         
         <p v-if="error" class="has-text-danger has-text-centered mt-3 mb-4">{{ error }}</p>
-        
-
       </form>
       
       <!-- Bottone Add Points spostato in fondo alla pagina -->
@@ -62,7 +168,7 @@
           <span class="icon">
             <i class="fas fa-plus"></i>
           </span>
-          <span>Add Points</span>
+          <span>Aggiungi Punti</span>
         </button>
       </div>
     </div>
@@ -101,12 +207,17 @@ export default {
   data() {
     return {
       selectedTeam: '',
+      selectedCycle: '',
       selectedGame: '',
       pointsToAdd: 0,
       successMessage: '',
       showSuccessModal: false,
       modalClosing: false,
       overlayColor: '',
+      showTeamDropdown: false,
+      showCycleDropdown: false,
+      showGameDropdown: false,
+      showPointsDropdown: false,
       teams: [
         { id: 'rossi', name: 'Rossi', points: 0, color: '#FF0000' },
         { id: 'verdi', name: 'Verdi', points: 0, color: '#008000' },
@@ -115,35 +226,63 @@ export default {
         { id: 'fucsia', name: 'Fucsia', points: 0, color: '#FF00FF' },
         { id: 'gialli', name: 'Gialli', points: 0, color: '#FFFF00' }
       ],
-      games: [
-        { id: 'bandierina', name: 'Bandierina' },
-        { id: 'lupo-ghiaccio', name: 'Lupo ghiaccio' },
-        { id: 'uovo-in-bilico', name: 'Uovo in bilico' },
-        { id: 'cerchi-nelle-aste', name: 'Cerchi nelle aste' },
-        { id: 'lupo-coda', name: 'Lupo coda' },
-        { id: 'terra-acqua', name: 'Terra/acqua' },
-        { id: 'rispetta-le-tappe', name: 'Rispetta le tappe' },
-        { id: 'calcio', name: 'Calcio' },
-        { id: 'tiro-alla-fune', name: 'Tiro alla fune' },
-        { id: 'bandierina-mix', name: 'Bandierina mix' },
-        { id: 'castelli', name: 'Castelli' },
-        { id: 'pallamano', name: 'Pallamano' },
-        { id: 'un-difficile-viaggio', name: 'Un difficile viaggio' },
-        { id: 'palla-prigioniera', name: 'Palla prigioniera' },
-        { id: 'tchoukball', name: 'Tchoukball' },
-        { id: 'passa-la-linea', name: 'Passa la linea' },
-        { id: '10-tappe', name: '10 tappe' },
-        { id: 'basket', name: 'Basket' }
-      ],
+      gamesByCycle: {
+        primo: [
+          { id: "bandierina", name: "Bandierina" },
+          { id: "lupo-ghiaccio", name: "Lupo ghiaccio" },
+          { id: "aste-in-movimento", name: "Aste in movimento" },
+          { id: "lupo-coda", name: "Lupo coda" },
+          { id: "staffetta-a-ostacoli-i-ciclo", name: "Staffetta a ostacoli (I Ciclo)" },
+          { id: "sfonda-la-porta", name: "Sfonda la porta" },
+          { id: "nella-palude-tronco-o-liana", name: "Nella Palude (Tronco o Liana)" },
+          { id: "domina-i-cerchi", name: "Domina I cerchi" }
+        ],
+        secondo: [
+          { id: "tiro-alla-fune", name: "Tiro alla fune" },
+          { id: "bandierina-mix", name: "Bandierina mix" },
+          { id: "castelli", name: "Castelli" },
+          { id: "pallamano", name: "Pallamano" },
+          { id: "staffetta-a-ostacoli-ii-ciclo", name: "Staffetta a ostacoli (II Ciclo)" },
+          { id: "lupo-ghiaccio", name: "Lupo ghiaccio" },
+          { id: "aste-in-movimento", name: "Aste in movimento" },
+          { id: "lupo-coda", name: "Lupo coda" },
+          { id: "palla-prigioniera", name: "Palla prigioniera" },
+          { id: "tchoukball", name: "Tchoukball" },
+          { id: "nella-palude-tronco-o-liana", name: "Nella Palude (Tronco o Liana)" },
+          { id: "10-tappe", name: "10 tappe" }
+        ],
+        terzo: [
+          { id: "dai-fai-canestro", name: "Dai… Fai canestro" },
+          { id: "pallamano", name: "Pallamano" },
+          { id: "castelli", name: "Castelli" },
+          { id: "tchoukball", name: "Tchoukball" },
+          { id: "tiro-alla-fune", name: "Tiro alla fune" },
+          { id: "10-tappe", name: "10 tappe" },
+          { id: "palla-prigioniera", name: "Palla prigioniera" },
+          { id: "bandierina-mix", name: "Bandierina mix" },
+          { id: "calcetto", name: "Calcetto" }
+        ]
+      },
       pointsOptions: [0, 20, 40, 80],
       error: ''
     };
   },
+  computed: {
+    availableGames() {
+      return this.selectedCycle ? this.gamesByCycle[this.selectedCycle] : [];
+    }
+  },
+  watch: {
+    selectedCycle() {
+      // Reset game selection when cycle changes
+      this.selectedGame = '';
+    }
+  },
   methods: {
     async addPoints() {
       try {
-        if (!this.selectedTeam || !this.selectedGame || this.pointsToAdd <= 0) {
-          this.error = 'Please fill in all fields and add at least one point.';
+        if (!this.selectedTeam || !this.selectedCycle || !this.selectedGame || this.pointsToAdd <= 0) {
+          this.error = 'Compila tutti i campi e aggiungi almeno un punto.';
           return;
         }
 
@@ -163,7 +302,7 @@ export default {
           // Messaggio di successo con modal
           const teamName = this.teams.find(t => t.id === this.selectedTeam)?.name;
           const teamColor = this.teams.find(t => t.id === this.selectedTeam)?.color;
-          const gameName = this.games.find(g => g.id === this.selectedGame)?.name;
+          const gameName = this.availableGames.find(g => g.id === this.selectedGame)?.name;
           this.successMessage = `${this.pointsToAdd} punti aggiunti al team ${teamName} per ${gameName}`;
           this.error = '';
           
@@ -176,6 +315,7 @@ export default {
           
           // Reset form
           this.selectedTeam = '';
+          this.selectedCycle = '';
           this.selectedGame = '';
           this.pointsToAdd = 0;
           
@@ -212,9 +352,10 @@ export default {
 
         await updateDoc(docRef, {
           history: arrayUnion({
-            username: getCurrentUser() ? getCurrentUser().username : 'unknown',
+            username: getCurrentUser() ? getCurrentUser().username : 'sconosciuto',
             team: this.selectedTeam,
             game: this.selectedGame,
+            cycle: this.selectedCycle,
             points: this.pointsToAdd,
             timestamp: new Date()
           })
@@ -222,7 +363,75 @@ export default {
       } else {
         this.error = 'Points document does not exist.';
       }
-    }
+    },
+    
+    // Metodi per gestire i dropdown personalizzati
+    toggleTeamDropdown() {
+      this.showTeamDropdown = !this.showTeamDropdown;
+      this.closeOtherDropdowns('team');
+    },
+    
+    toggleCycleDropdown() {
+      this.showCycleDropdown = !this.showCycleDropdown;
+      this.closeOtherDropdowns('cycle');
+    },
+    
+    toggleGameDropdown() {
+      if (!this.selectedCycle) return;
+      this.showGameDropdown = !this.showGameDropdown;
+      this.closeOtherDropdowns('game');
+    },
+    
+    togglePointsDropdown() {
+      this.showPointsDropdown = !this.showPointsDropdown;
+      this.closeOtherDropdowns('points');
+    },
+    
+    selectTeam(teamId) {
+      this.selectedTeam = teamId;
+      this.showTeamDropdown = false;
+    },
+    
+    selectCycle(cycleId) {
+      this.selectedCycle = cycleId;
+      this.selectedGame = ''; // Reset game when cycle changes
+      this.showCycleDropdown = false;
+    },
+    
+    selectGame(gameId) {
+      this.selectedGame = gameId;
+      this.showGameDropdown = false;
+    },
+    
+    selectPoints(points) {
+      this.pointsToAdd = points;
+      this.showPointsDropdown = false;
+    },
+    
+    getCycleName(cycle) {
+      const names = {
+        primo: 'Primo Ciclo',
+        secondo: 'Secondo Ciclo',
+        terzo: 'Terzo Ciclo'
+      };
+      return names[cycle] || '';
+    },
+    
+    closeOtherDropdowns(except) {
+      if (except !== 'team') this.showTeamDropdown = false;
+      if (except !== 'cycle') this.showCycleDropdown = false;
+      if (except !== 'game') this.showGameDropdown = false;
+      if (except !== 'points') this.showPointsDropdown = false;
+    },
+    
+    closeDropdowns(event) {
+      if (!event.target.closest('.custom-dropdown')) {
+        this.showTeamDropdown = false;
+        this.showCycleDropdown = false;
+        this.showGameDropdown = false;
+        this.showPointsDropdown = false;
+      }
+    },
   }
 }
 </script>
@@ -233,6 +442,114 @@ export default {
   font-weight: bold !important;
   color: #363636 !important;
   margin: 0 !important;
+}
+
+/* Custom Dropdown Styles */
+.custom-dropdown {
+  position: relative;
+  width: 100%;
+}
+
+.custom-dropdown.is-disabled {
+  opacity: 0.6;
+  pointer-events: none;
+}
+
+.dropdown-trigger {
+  cursor: pointer;
+  width: 100%;
+}
+
+.dropdown-display {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.75rem 1rem;
+  border: 2px solid #e3e3e3;
+  border-radius: 8px;
+  background: white;
+  transition: all 0.3s ease;
+  min-height: 3rem;
+}
+
+.custom-dropdown.is-active .dropdown-display {
+  border-color: #667eea;
+  box-shadow: 0 0 0 0.125em rgba(102, 126, 234, 0.25);
+}
+
+.dropdown-display:hover {
+  border-color: #667eea;
+}
+
+.placeholder {
+  color: #999;
+  font-style: italic;
+}
+
+.selected-option {
+  color: #333;
+  font-weight: 500;
+}
+
+.selected-team {
+  font-weight: bold;
+}
+
+.points-display {
+  color: #667eea;
+  font-weight: 600;
+}
+
+.dropdown-icon {
+  color: #999;
+  transition: transform 0.3s ease;
+}
+
+.custom-dropdown.is-active .dropdown-icon {
+  transform: rotate(180deg);
+}
+
+.dropdown-menu {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  margin-top: 4px;
+}
+
+.dropdown-content {
+  background: white;
+  border-radius: 8px;
+  box-shadow: 0 8px 32px rgba(0, 0, 0, 0.1);
+  border: 1px solid #e3e3e3;
+  max-height: 300px;
+  overflow-y: auto;
+}
+
+.dropdown-item {
+  padding: 0.75rem 1rem;
+  cursor: pointer;
+  transition: all 0.2s ease;
+  border-bottom: 1px solid #f5f5f5;
+}
+
+.dropdown-item:last-child {
+  border-bottom: none;
+}
+
+.dropdown-item:hover {
+  background-color: #f8f9fa;
+}
+
+.dropdown-item.is-active {
+  background-color: #667eea;
+  color: white;
+}
+
+.points-item.is-active {
+  background-color: #667eea;
+  color: white;
 }
 
 .add-button-section {
@@ -278,31 +595,10 @@ export default {
   transition: all 0.3s ease;
 }
 
-.select select {
-  border-radius: 8px;
-  border: 2px solid #e3e3e3;
-  transition: all 0.3s ease;
-  height: auto;
-  padding: 0.75rem;
-  font-size: 1rem;
-}
-
-.select.is-fullwidth {
-  width: 100%;
-}
-
-.select.is-fullwidth select {
-  width: 100%;
-}
-
-.select select:focus {
-  border-color: #667eea;
-  box-shadow: 0 0 0 0.125em rgba(102, 126, 234, 0.25);
-}
-
 .label {
   font-weight: 600;
   color: #4a4a4a;
+  margin-bottom: 0.75rem;
 }
 
 .field {
@@ -350,28 +646,36 @@ export default {
     color: #ffffff;
   }
   
-  .select select {
+  .dropdown-display {
     background-color: #3a3a3a;
     border-color: #555555;
     color: #ffffff;
   }
   
-  .select select:focus {
-    background-color: #404040;
-    border-color: #667eea;
+  .dropdown-content {
+    background-color: #3a3a3a;
+    border-color: #555555;
   }
   
-  .select::after {
-    border-color: #ffffff;
+  .dropdown-item {
+    color: #ffffff;
+    border-bottom-color: #555555;
+  }
+  
+  .dropdown-item:hover {
+    background-color: #4a4a4a;
+  }
+  
+  .placeholder {
+    color: #aaa;
+  }
+  
+  .selected-option {
+    color: #ffffff;
   }
   
   .page-title {
     color: #ffffff !important;
-  }
-  
-  .bianchi-text.team-option {
-    color: #CCCCCC !important;
-    background-color: #2a2a2a;
   }
 }
 
@@ -401,15 +705,10 @@ export default {
     margin-bottom: 0.5rem;
   }
   
-  .select select {
+  .dropdown-display {
     padding: 0.75rem;
     font-size: 1rem;
     line-height: 1.4;
-  }
-  
-  .button.is-medium {
-    font-size: 1.1rem;
-    padding: 1rem 2rem;
   }
   
   .button.is-danger.is-small {
@@ -443,6 +742,10 @@ export default {
   
   .field {
     margin-bottom: 0.75rem;
+  }
+  
+  .dropdown-content {
+    max-height: 200px;
   }
 }
 
